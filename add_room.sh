662f9a199b63e3e9f950f62d6967484886ad9132
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-05-07 03:23:22 +0900

    update shell scripts

--- add_room.sh
+++ add_room.sh
@@ -7,9 +7,11 @@ VERBOSE="$3"
 HOST="localhost:8080"
 URI="http://$HOST/rooms"
 BODY="{\"user_id\": \"$USER_ID\", \"room_name\": \"bar\"}"
+HA="Content-Type: application/json"
+HB="Cookie: session=$SESSION"
 
 if [ "$VERBOSE" = "" ]; then
-  curl -X POST -H "Content-Type: application/json" -H "Cookie: session=$SESSION" -d "$BODY" "$URI"
+  curl -X POST -H "$HA" -H "$HB" -d "$BODY" "$URI" | jq -r '.room_id'
 else
-  curl -v -X POST -H "Content-Type: application/json" -H "Cookie: session=$SESSION" -d "$BODY" "$URI"
+  curl -v -X POST -H "$HA" -H "$HB" -d "$BODY" "$URI" | jq -r '.room_id'
 fi
--- add_user.sh
+++ add_user.sh
@@ -7,7 +7,7 @@ URI="http://$HOST/users"
 BODY='{"user_name": "Taro"}'
 
 if [ "$VERBOSE" = "" ]; then
-  curl -X POST -H "Content-Type: application/json" -d "$BODY" "$URI"
+  curl -X POST -H "Content-Type: application/json" -d "$BODY" "$URI" | jq -r '.user_id'
 else
-  curl -v -X POST -H "Content-Type: application/json" -d "$BODY" "$URI"
+  curl -v -X POST -H "Content-Type: application/json" -d "$BODY" "$URI" | jq -r '.user_id'
 fi
